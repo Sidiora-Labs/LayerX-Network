0c18f7ecc2d882eefcf016051758f1a91328bc0a
Sidiora Development Team <andresrodriguez11005@gmail.com>
2026-09-13 01:04:30 +0200

    Exercise access list journal rollback with complete slot storage
    Co-Authored-By: Sidiora Development Team <andresrodriguez11005@gmail.com>

--- paxeer-network/modules/evm/state/journal_slots_test.go
+++ paxeer-network/modules/evm/state/journal_slots_test.go
@@ -13,22 +13,26 @@ func TestAccessListSlotRollbackPreservesOtherAddressIndexes(t *testing.T) {
 		addresses := []common.Address{{1}, {2}, {3}}
 		slots := []common.Hash{{11}, {12}, {13}}
 		for i, address := range addresses {
-			db.AddSlotToAccessList(address, slots[i])
+			db.tempState.transientAccessLists.Addresses[address] = i
+			db.tempState.transientAccessLists.Slots = append(db.tempState.transientAccessLists.Slots, map[common.Hash]struct{}{slots[i]: {}})
 		}
 		(&accessListAddSlotChange{address: addresses[removed], slot: slots[removed]}).revert(db)
 		for i, address := range addresses {
-			addressFound, slotFound := db.SlotInAccessList(address, slots[i])
+			index, addressFound := db.tempState.transientAccessLists.Addresses[address]
 			require.True(t, addressFound)
-			require.Equal(t, i != removed, slotFound)
+			if i == removed {
+				require.Equal(t, -1, index)
+			} else {
+				require.Contains(t, db.tempState.transientAccessLists.Slots[index], slots[i])
+				require.Len(t, db.tempState.transientAccessLists.Slots[index], 1)
+			}
 		}
 		for i := len(addresses) - 1; i >= 0; i-- {
 			if i == removed {
 				continue
 			}
 			(&accessListAddSlotChange{address: addresses[i], slot: slots[i]}).revert(db)
-			addressFound, slotFound := db.SlotInAccessList(addresses[i], slots[i])
-			require.True(t, addressFound)
-			require.False(t, slotFound)
+			require.Equal(t, -1, db.tempState.transientAccessLists.Addresses[addresses[i]])
 		}
 		require.Empty(t, db.tempState.transientAccessLists.Slots)
 		for _, address := range addresses {
